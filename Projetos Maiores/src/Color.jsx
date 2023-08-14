0a7779d64855079997febdb890850d67821c0f94
Wesley Macedo <123600728+wesleymacedodev@users.noreply.github.com>
2023-08-14 00:13:51 -0300

    Delete Projetos Maiores/src directory

--- Projetos Maiores/src/Color.jsx
+++
@@ -1,6 +0,0 @@
-const colorPrimary = "#472D2D"
-const colorSecundary = "#553939"
-const colorTertiary = "#704F4F"
-const colorText = "#A77979"
-
-export { colorPrimary, colorSecundary, colorTertiary, colorText }
